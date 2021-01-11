107e59d7579594486fc06bf350beb9e5d5e058c1
retroscilo <loic.torres@hetic.net>
2021-01-11 21:48:51 +0100

    test

--- dist/loader.b9d7b1b4.js
+++ dist/loader.b9d7b1b4.js
@@ -5712,7 +5712,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "63407" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "60208" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
