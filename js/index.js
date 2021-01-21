ca85fbafd9431accd4fae5fefaa3664fcc7c3373
retroscilo <loic.torres@hetic.net>
2021-01-21 17:27:37 +0100

    prevent safari zoom

--- js/index.js
+++ js/index.js
@@ -47,4 +47,15 @@ import FormHandler from './lib/invitation.js';
     section1.querySelector(' .background').remove();
     section1.querySelector(' .foreground').remove();
   }
+
+  var ua = window.navigator.userAgent;
+  var iOS = !!ua.match(/iPad/i) || !!ua.match(/iPhone/i);
+  var webkit = !!ua.match(/WebKit/i);
+  var iOSSafari = iOS && webkit && !ua.match(/CriOS/i);
+  if(iOSSafari) {
+    let meta = document.createElement('meta');
+    meta.name =  "viewport";
+    meta.content = "width=device-width, user-scalable=no";
+    document.getElementsByTagName('head')[0].appendChild(meta);
+  }
 }) ()
