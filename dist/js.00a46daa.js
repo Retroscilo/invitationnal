d7eab5f105028800e7d0d849385256eddc8d0927
Loïc Torres <loic.torresg@gmail.com>
2022-03-05 11:15:09 +0100

    modifs

--- dist/js.00a46daa.js
+++ dist/js.00a46daa.js
@@ -13087,7 +13087,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49458" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "57383" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
